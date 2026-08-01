9853f6542cfa8938674da844357f4afa0914324e
mohammed ghaleb eltaher <elsamerymohamed445@gmail.com>
2026-08-02 02:46:30 +0300

    Create ROBOT_CONFIG.h

---
+++ Firmware/master/inc/ROBOT_CONFIG.h
@@ -0,0 +1,33 @@
+
+
+#ifndef ROBOT_CONFIG_H_
+#define ROBOT_CONFIG_H_
+
+#include "DIO.h"
+
+
+
+#define LCD_RS_PORT PORTC_ID
+#define LCD_RS_PIN  PIN6
+
+#define LCD_RW_PORT PORTC_ID
+#define LCD_RW_PIN  PIN5
+
+#define LCD_EN_PORT PORTC_ID
+#define LCD_EN_PIN  PIN7
+
+#define LCD_DATA_PORT PORTD_ID
+
+
+
+#define KEYPAD_PORT PORTA_ID
+
+
+
+#define UART_BAUD 9600
+
+
+#define BT_NAME "MedicalRobot"
+
+#endif
+
