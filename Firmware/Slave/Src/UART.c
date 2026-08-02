6f0f4ea02988c29fdcd0a4d637a8e59329f3232c
mohammed ghaleb eltaher <elsamerymohamed445@gmail.com>
2026-08-02 03:05:52 +0300

    Create UART.c

---
+++ Firmware/Slave/Src/UART.c
@@ -0,0 +1,55 @@
+#include <avr/io.h>
+
+#include "UART.h"
+#include "BIT_MATH.h"
+
+/* Baud = 9600 @ F_CPU = 8MHz -> UBRR = 51 (??? ???? ??????? ??????) */
+#define UART_UBRR_VALUE 51
+
+void UART_Init(void)
+{
+	UCSRA = 0x00;
+
+	/* ????? ????????? (RXEN) */
+	SET_BIT(UCSRB, RXEN);
+
+	/* ????? ??????? (TXEN) */
+	SET_BIT(UCSRB, TXEN);
+
+	/* URSEL ???? = 1 ???? ???? ??? UCSRC (?? UBRRH) - ?????? ???? ?? ATmega32 */
+	SET_BIT(UCSRC, URSEL);
+
+	/* UCSZ1 + UCSZ0 = 1 -> ??? ???????? 8-bit */
+	SET_BIT(UCSRC, UCSZ1);
+	SET_BIT(UCSRC, UCSZ0);
+
+	UBRRH = 0;
+	UBRRL = UART_UBRR_VALUE;
+}
+
+void UART_SendChar(u8 data)
+{
+	while(GET_BIT(UCSRA, UDRE) == 0);
+
+	UDR = data;
+}
+
+void UART_SendString(u8 *str)
+{
+	while(*str)
+	{
+		UART_SendChar(*str++);
+	}
+}
+
+u8 UART_ReceiveChar(void)
+{
+	while(GET_BIT(UCSRA, RXC) == 0);
+
+	return UDR;
+}
+
+u8 UART_DataAvailable(void)
+{
+	return GET_BIT(UCSRA, RXC);
+}
